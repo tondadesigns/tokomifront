60677768aaf8d5b11db165f152a199f660e5e770
tondadesigns <kevin.djamboleka@gmail.com>
2025-09-09 03:13:06 +0200

    Update app/product/[slug]/page.tsx

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,1 +1,24 @@
+import React from "react";
+import ProductDetail from "@/components/product-detail";
+
+export default function ProductPage({ params }: { params: { slug: string } }) {
+
+  const product = {
+    id: params.slug,
+    name: "HOODIE TOKOMI",
+    price: 149,
+    images: [
+      "/images/hoodie-avant.png",
+      "/images/hoodie-profil.png",
+      "/images/hoodie-arriere.png",
+    ],
+    sizes: ["XS", "S", "M", "L", "XL"],
+    colors: [
+      { code: "black", label: "Noir", swatch: "#000000" },
+      { code: "white", label: "Blanc", swatch: "#ffffff" },
+    ],
+  };
+
+  return <ProductDetail product={product} />;
+}
 
