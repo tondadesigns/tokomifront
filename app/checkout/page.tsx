910a8f28f5ab9fc7d1be03f3600773c3f52c28aa
tondadesigns <kevin.djamboleka@gmail.com>
2025-09-16 21:53:26 +0200

    Update app/checkout/page.tsx

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,1 +1,13 @@
+import AuthGate from "@/components/AuthGate";
+import CheckoutClient from "./CheckoutClient";
+
+export const metadata = { title: "Checkout – Tokomi" };
+
+export default function Page() {
+  return (
+    <AuthGate>
+      <CheckoutClient />
+    </AuthGate>
+  );
+}
 
