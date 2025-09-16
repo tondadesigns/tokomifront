2165257cdfdcb900c49726a4960ca7a07e07f190
tondadesigns <kevin.djamboleka@gmail.com>
2025-09-16 22:06:03 +0200

    Update page.tsx

--- app/checkout/shipping/page.tsx
+++ app/checkout/shipping/page.tsx
@@ -1,1 +1,13 @@
+import AuthGate from "@/components/AuthGate";
+import ShippingClient from "./ShippingClient";
+
+export const metadata = { title: "Expédition – Tokomi" };
+
+export default function Page() {
+  return (
+    <AuthGate>
+      <ShippingClient />
+    </AuthGate>
+  );
+}
 
