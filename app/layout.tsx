ead8112fea4efabce1fa60f017ae162020d7ad7b
tondadesigns <kevin.djamboleka@gmail.com>
2025-10-02 23:57:54 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,9 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
         <html lang="en">
         <body>
-        <CartProvider>
+
         {children}
-        </CartProvider>
+
         </body>
         </html>
     );
